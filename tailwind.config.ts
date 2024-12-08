6ce9369a9dffcac996a38f7893505afa636e9789
Shayan Baloch <shayanbalochofficial@gmail.com>
2024-12-09 01:29:02 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "great-vibes": ['"Great Vibes"', "cursive"], // Add Great Vibes here
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
