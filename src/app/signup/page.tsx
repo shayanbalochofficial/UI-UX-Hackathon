d2325f1feadc05469fb20638ce0179e48680f20d
Shayan Baloch <shayanbalochofficial@gmail.com>
2024-12-09 18:45:07 +0500

    page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,9 @@
-// SignInPage
+
 import Navbar from "@/components/Nav2";
 // import Image from "next/image";/
 import Link from "next/link";
 import Header from "./header";
 import { FaGoogle, FaApple } from "react-icons/fa";
-// import { IoChevronForwardOutline } from "react-icons/io5";
 
 export default function SignInPage() {
   return (
@@ -13,7 +12,7 @@ export default function SignInPage() {
       <Navbar />
       <Header />
 
-      {/* Signin Form */}
+      {/* Signup Form */}
       <section className="py-16">
         <div className="container mx-auto max-w-md bg-white shadow-lg rounded-md p-8">
           <h3 className="text-2xl font-bold mb-6 text-center">Sign Up</h3>
